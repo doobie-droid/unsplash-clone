eb57a1aea0d0374d82014f790b9d5477bff56d28
doobie-droid <lesliedouglas23@gmail.com>
2025-02-17 13:19:36 +0100

    feat: scattered grid has been implemented

--- pages/index.vue
+++ pages/index.vue
@@ -55,6 +55,207 @@
         </div>
       </form>
     </div>
-    <div class="index-body">Body</div>
+    <div class="index-body">
+      <div class="picture-display">
+        <div class="picture-carousel">
+          <figure
+            class="picture-card"
+            itemprop="image"
+            itemscope
+            itemtype="https://schema.org/ImageObject"
+          >
+            <a
+              itemprop="imageUrl"
+              title="The inside of a computer case lit up at night"
+              href="/photos/a-person-sitting-in-the-doorway-of-a-restaurant-3XMMBl5W4C4"
+              ><div>
+                <!-- <img
+                  src="data:image/bmp;base64,Qk32BAAAAAAAADYAAAAoAAAACAAAAAgAAAABABgAAAAAAMAAAAATCwAAEwsAAAAAAAAAAAAAAgAABgAACwEADwgAEw0pGhY3IyArKygAGhgjGBkgGB8pICpILTVjNDtsNThcMDE0Jic/Iic+IC1JKztnPEqCRE+LQUh4NDdMKSpHJStHIjFUL0FxQFCMSVWUREyBNDhTJCU/ISVAHyxNKztoOkiAQUyHPUR0LjJJFBIjFBUnFh42HylMKDJeLDRhKi9RIyQrAAAAAAAACAAACgAOAAAQAAAAAgAAFA4AAAAAAAAAAAAAAAAAAAAAAAAAAAAACgAA"
+                /> -->
+                <img
+                  alt="A person sitting in the doorway of a restaurant"
+                  loading="lazy"
+                  itemprop="thumbnailUrl"
+                  sizes="(min-width: 1440px) 420px, (min-width: 992px) calc(calc(100vw - 88px) / 3), (min-width: 768px) calc(calc(100vw - 64px) / 2), 100vw"
+                  srcset="
+                    https://images.unsplash.com/photo-1739641375724-dfea74e0df69?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3MTAzMjZ8MHwxfGFsbHwxfHx8fHx8fHwxNzM5NzUxNTcyfA&ixlib=rb-4.0.3&q=80&w=200 200w,
+                    https://images.unsplash.com/photo-1739641375724-dfea74e0df69?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3MTAzMjZ8MHwxfGFsbHwxfHx8fHx8fHwxNzM5NzUxNTcyfA&ixlib=rb-4.0.3&q=80&w=400 400w
+                  "
+                /></div
+            ></a>
+          </figure>
+          <figure
+            class="picture-card"
+            itemprop="image"
+            itemscope
+            itemtype="https://schema.org/ImageObject"
+          >
+            <a
+              itemprop="imageUrl"
+              title="A city in the sun"
+              href="/photos/the-inside-of-a-computer-case-lit-up-at-night-3XMMBl5W4C4"
+              ><div>
+                <!-- <img
+                  src="data:image/bmp;base64,Qk32BAAAAAAAADYAAAAoAAAACAAAAAgAAAABABgAAAAAAMAAAAATCwAAEwsAAAAAAAAAAAAAAgAABgAACwEADwgAEw0pGhY3IyArKygAGhgjGBkgGB8pICpILTVjNDtsNThcMDE0Jic/Iic+IC1JKztnPEqCRE+LQUh4NDdMKSpHJStHIjFUL0FxQFCMSVWUREyBNDhTJCU/ISVAHyxNKztoOkiAQUyHPUR0LjJJFBIjFBUnFh42HylMKDJeLDRhKi9RIyQrAAAAAAAACAAACgAOAAAQAAAAAgAAFA4AAAAAAAAAAAAAAAAAAAAAAAAAAAAACgAA"
+                /> -->
+                <img
+                  alt="A city in the sun"
+                  loading="lazy"
+                  itemprop="thumbnailUrl"
+                  sizes="(min-width: 1440px) 420px, (min-width: 992px) calc(calc(100vw - 88px) / 3), (min-width: 768px) calc(calc(100vw - 64px) / 2), 100vw"
+                  srcset="
+                    https://plus.unsplash.com/premium_photo-1734607187630-f5dba4be6f6d?w=100&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8 100w,
+                    https://plus.unsplash.com/premium_photo-1734607187630-f5dba4be6f6d?w=200&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8 200w,
+                    https://plus.unsplash.com/premium_photo-1734607187630-f5dba4be6f6d?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8 400w,
+                    https://plus.unsplash.com/premium_photo-1734607187630-f5dba4be6f6d?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8 800w
+                  "
+                /></div
+            ></a>
+          </figure>
+          <figure
+            class="picture-card"
+            itemprop="image"
+            itemscope
+            itemtype="https://schema.org/ImageObject"
+          >
+            <a
+              itemprop="imageUrl"
+              title="A person taking a picture of a city"
+              href="/photos/the-inside-of-a-computer-case-lit-up-at-night-3XMMBl5W4C4"
+              ><div>
+                <!-- <img
+                  src="data:image/bmp;base64,Qk32BAAAAAAAADYAAAAoAAAACAAAAAgAAAABABgAAAAAAMAAAAATCwAAEwsAAAAAAAAAAAAAAgAABgAACwEADwgAEw0pGhY3IyArKygAGhgjGBkgGB8pICpILTVjNDtsNThcMDE0Jic/Iic+IC1JKztnPEqCRE+LQUh4NDdMKSpHJStHIjFUL0FxQFCMSVWUREyBNDhTJCU/ISVAHyxNKztoOkiAQUyHPUR0LjJJFBIjFBUnFh42HylMKDJeLDRhKi9RIyQrAAAAAAAACAAACgAOAAAQAAAAAgAAFA4AAAAAAAAAAAAAAAAAAAAAAAAAAAAACgAA"
+                /> -->
+                <img
+                  alt="A person taking a picture of a city"
+                  loading="lazy"
+                  itemprop="thumbnailUrl"
+                  sizes="(min-width: 1440px) 420px, (min-width: 992px) calc(calc(100vw - 88px) / 3), (min-width: 768px) calc(calc(100vw - 64px) / 2), 100vw"
+                  srcset="
+                    https://images.unsplash.com/photo-1739590485766-fad3dadd2056?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3MTAzMjZ8MHwxfGFsbHwzfHx8fHx8fHwxNzM5NzUxNTcyfA&ixlib=rb-4.0.3&q=80&w=100 100w,
+                    https://images.unsplash.com/photo-1739590485766-fad3dadd2056?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3MTAzMjZ8MHwxfGFsbHwzfHx8fHx8fHwxNzM5NzUxNTcyfA&ixlib=rb-4.0.3&q=80&w=200 200w,
+                    https://images.unsplash.com/photo-1739590485766-fad3dadd2056?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3MTAzMjZ8MHwxfGFsbHwzfHx8fHx8fHwxNzM5NzUxNTcyfA&ixlib=rb-4.0.3&q=80&w=400 400w,
+                    https://images.unsplash.com/photo-1739590485766-fad3dadd2056?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3MTAzMjZ8MHwxfGFsbHwzfHx8fHx8fHwxNzM5NzUxNTcyfA&ixlib=rb-4.0.3&q=80&w=800 800w
+                  "
+                /></div
+            ></a>
+          </figure>
+          <figure
+            class="picture-card"
+            itemprop="image"
+            itemscope
+            itemtype="https://schema.org/ImageObject"
+          >
+            <a
+              itemprop="imageUrl"
+              title="A person holding a flower in their hand"
+              href="/photos/the-inside-of-a-computer-case-lit-up-at-night-3XMMBl5W4C4"
+              ><div>
+                <!-- <img
+                  src="data:image/bmp;base64,Qk32BAAAAAAAADYAAAAoAAAACAAAAAgAAAABABgAAAAAAMAAAAATCwAAEwsAAAAAAAAAAAAAAgAABgAACwEADwgAEw0pGhY3IyArKygAGhgjGBkgGB8pICpILTVjNDtsNThcMDE0Jic/Iic+IC1JKztnPEqCRE+LQUh4NDdMKSpHJStHIjFUL0FxQFCMSVWUREyBNDhTJCU/ISVAHyxNKztoOkiAQUyHPUR0LjJJFBIjFBUnFh42HylMKDJeLDRhKi9RIyQrAAAAAAAACAAACgAOAAAQAAAAAgAAFA4AAAAAAAAAAAAAAAAAAAAAAAAAAAAACgAA"
+                /> -->
+                <img
+                  alt="A person holding a flower in their hand"
+                  loading="lazy"
+                  itemprop="thumbnailUrl"
+                  sizes="(min-width: 1440px) 420px, (min-width: 992px) calc(calc(100vw - 88px) / 3), (min-width: 768px) calc(calc(100vw - 64px) / 2), 100vw"
+                  srcset="
+                    https://images.unsplash.com/photo-1739641375724-dfea74e0df69?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3MTAzMjZ8MHwxfGFsbHwxfHx8fHx8fHwxNzM5NzUxNTcyfA&ixlib=rb-4.0.3&q=80&w=100 100w,
+                    https://images.unsplash.com/photo-1739632141610-018cfc9e60ef?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3MTAzMjZ8MHwxfGFsbHw0fHx8fHx8fHwxNzM5NzUxNTcyfA&ixlib=rb-4.0.3&q=80&w=200 200w,
+                    https://images.unsplash.com/photo-1739632141610-018cfc9e60ef?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3MTAzMjZ8MHwxfGFsbHw0fHx8fHx8fHwxNzM5NzUxNTcyfA&ixlib=rb-4.0.3&q=80&w=400 400w,
+                    https://images.unsplash.com/photo-1739632141610-018cfc9e60ef?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3MTAzMjZ8MHwxfGFsbHw0fHx8fHx8fHwxNzM5NzUxNTcyfA&ixlib=rb-4.0.3&q=80&w=800 800w
+                  "
+                /></div
+            ></a>
+          </figure>
+          <figure
+            class="picture-card"
+            itemprop="image"
+            itemscope
+            itemtype="https://schema.org/ImageObject"
+          >
+            <a
+              itemprop="imageUrl"
+              title="The inside of a computer case lit up at night"
+              href="/photos/a-person-sitting-in-the-doorway-of-a-restaurant-3XMMBl5W4C4"
+              ><div>
+                <!-- <img
+                  src="data:image/bmp;base64,Qk32BAAAAAAAADYAAAAoAAAACAAAAAgAAAABABgAAAAAAMAAAAATCwAAEwsAAAAAAAAAAAAAAgAABgAACwEADwgAEw0pGhY3IyArKygAGhgjGBkgGB8pICpILTVjNDtsNThcMDE0Jic/Iic+IC1JKztnPEqCRE+LQUh4NDdMKSpHJStHIjFUL0FxQFCMSVWUREyBNDhTJCU/ISVAHyxNKztoOkiAQUyHPUR0LjJJFBIjFBUnFh42HylMKDJeLDRhKi9RIyQrAAAAAAAACAAACgAOAAAQAAAAAgAAFA4AAAAAAAAAAAAAAAAAAAAAAAAAAAAACgAA"
+                /> -->
+                <img
+                  alt="A person sitting in the doorway of a restaurant"
+                  loading="lazy"
+                  itemprop="thumbnailUrl"
+                  sizes="(min-width: 1440px) 420px, (min-width: 992px) calc(calc(100vw - 88px) / 3), (min-width: 768px) calc(calc(100vw - 64px) / 2), 100vw"
+                  srcset="
+                    https://images.unsplash.com/photo-1739641375724-dfea74e0df69?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3MTAzMjZ8MHwxfGFsbHwxfHx8fHx8fHwxNzM5NzUxNTcyfA&ixlib=rb-4.0.3&q=80&w=200 200w,
+                    https://images.unsplash.com/photo-1739641375724-dfea74e0df69?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3MTAzMjZ8MHwxfGFsbHwxfHx8fHx8fHwxNzM5NzUxNTcyfA&ixlib=rb-4.0.3&q=80&w=400 400w
+                  "
+                /></div
+            ></a>
+          </figure>
+          <figure
+            class="picture-card"
+            itemprop="image"
+            itemscope
+            itemtype="https://schema.org/ImageObject"
+          >
+            <a
+              itemprop="imageUrl"
+              title="A city in the sun"
+              href="/photos/the-inside-of-a-computer-case-lit-up-at-night-3XMMBl5W4C4"
+              ><div>
+                <!-- <img
+                  src="data:image/bmp;base64,Qk32BAAAAAAAADYAAAAoAAAACAAAAAgAAAABABgAAAAAAMAAAAATCwAAEwsAAAAAAAAAAAAAAgAABgAACwEADwgAEw0pGhY3IyArKygAGhgjGBkgGB8pICpILTVjNDtsNThcMDE0Jic/Iic+IC1JKztnPEqCRE+LQUh4NDdMKSpHJStHIjFUL0FxQFCMSVWUREyBNDhTJCU/ISVAHyxNKztoOkiAQUyHPUR0LjJJFBIjFBUnFh42HylMKDJeLDRhKi9RIyQrAAAAAAAACAAACgAOAAAQAAAAAgAAFA4AAAAAAAAAAAAAAAAAAAAAAAAAAAAACgAA"
+                /> -->
+                <img
+                  alt="A city in the sun"
+                  loading="lazy"
+                  itemprop="thumbnailUrl"
+                  sizes="(min-width: 1440px) 420px, (min-width: 992px) calc(calc(100vw - 88px) / 3), (min-width: 768px) calc(calc(100vw - 64px) / 2), 100vw"
+                  srcset="
+                    https://plus.unsplash.com/premium_photo-1734607187630-f5dba4be6f6d?w=100&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8 100w,
+                    https://plus.unsplash.com/premium_photo-1734607187630-f5dba4be6f6d?w=200&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8 200w,
+                    https://plus.unsplash.com/premium_photo-1734607187630-f5dba4be6f6d?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8 400w,
+                    https://plus.unsplash.com/premium_photo-1734607187630-f5dba4be6f6d?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8 800w
+                  "
+                /></div
+            ></a>
+          </figure>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    console.log("Nuxt page mounted!");
+    window.addEventListener("resize", this.adjustGridRows);
+  },
+  methods: {
+    adjustGridRows() {
+      const gridItems = document.querySelectorAll(".picture-card");
+      gridItems.forEach((item) => {
+        const image = item.querySelector("img");
+        let rowSpan;
+        const height = image.clientHeight;
+        console.log(this.getMinRowHeight());
+        rowSpan = Math.round(height / this.getMinRowHeight());
+        item.style.gridRow = `span ${rowSpan}`;
+      });
+    },
+    getMinRowHeight() {
+      const gridItems = document.querySelectorAll(".picture-card");
+      let minHeight = 20000;
+      gridItems.forEach((item) => {
+        const image = item.querySelector("img");
+        const height = image.clientHeight;
+        minHeight = height < minHeight ? height : minHeight;
+      });
+      return minHeight;
+    },
+  },
+};
+</script>
