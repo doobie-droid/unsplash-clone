eaf7b868bcfe06bf2991d089f5ec0fce1a889d9b
doobie-droid <lesliedouglas23@gmail.com>
2025-02-19 05:49:00 +0100

    fix: test github_pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,12 @@ export default defineNuxtConfig({
       link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     },
   },
-
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ["/"],
+    },
+  },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["@/assets/styles/main.scss"],
