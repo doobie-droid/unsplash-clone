3fa4cb7bb4cb7734a890ae937ef5b1dce607abcb
doobie-droid <lesliedouglas23@gmail.com>
2025-03-20 00:21:31 +0100

    Revert "chore: checking how url responds in regard to nuxt.config"
    This reverts commit b1208cfcf7e850cb3062aa9f663ea72bb4525cd0.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: process.env.BASE_URL || "/unsplash-clon/",
+    baseURL: process.env.BASE_URL || "/unsplash-clone/",
     head: {
       title: process.env.npm_package_title || "",
       meta: [
